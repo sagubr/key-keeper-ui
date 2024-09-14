3025c5563c921de0afc2a866934aa0409df6975b
Gustavo Garcia <sousagustavogarcia@gmail.com>
2024-09-14 10:40:29 -0300

    first commit

---
+++ libs/openapi/config/custom-configuration.ts
@@ -0,0 +1,30 @@
+import { Injectable, Injector } from '@angular/core';
+import { Configuration } from '../configuration';
+import { AuthenticationService } from '@app/services/authentication.service';
+
+@Injectable()
+export class CustomConfiguration extends Configuration {
+  constructor(private readonly _injector: Injector) {
+    super({
+      credentials: {
+        Authorization: () => this._getAccessToken(),
+      },
+    });
+  }
+
+  private _getAuthenticationService(): AuthenticationService {
+    const service = this._injector.get(AuthenticationService);
+    this._getAuthenticationService = () => service;
+    return service;
+  }
+
+  private _getAccessToken(): string {
+    const token = this._getAuthenticationService().getToken();
+
+    if (!token) {
+      throw new Error('Token not found');
+    }
+
+    return token;
+  }
+}

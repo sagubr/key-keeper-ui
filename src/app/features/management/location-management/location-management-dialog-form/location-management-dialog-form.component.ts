a1958c0114c4085d851f6ed68ddc745d42c41022
Gustavo Garcia <sousagustavogarcia@gmail.com>
2024-11-13 15:29:14 -0300

    feat: ajuste lógica componentes
    - adicionado filters no componente cargos
    - adicionado filters e form no componente localizações (vazio)

---
+++ src/app/features/management/location-management/location-management-dialog-form/location-management-dialog-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-management-dialog-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './location-management-dialog-form.component.html',
+  styleUrl: './location-management-dialog-form.component.scss'
+})
+export class LocationManagementDialogFormComponent {
+
+}
---
+++ src/app/features/management/location-management/location-management-filters/location-management-filters.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-management-filters',
+  standalone: true,
+  imports: [],
+  templateUrl: './location-management-filters.component.html',
+  styleUrl: './location-management-filters.component.scss'
+})
+export class LocationManagementFiltersComponent {
+
+}
